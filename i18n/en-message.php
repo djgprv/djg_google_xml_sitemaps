b978ccafba4ed732ae5f946296de5cf240e8ba2e
djgprv <djgprv@gmail.com>
2013-04-21 19:37:57 +0200

    v.1.0.8

---
+++ i18n/en-message.php
@@ -0,0 +1,54 @@
+<?php
+
+    /**
+     * Polish file for plugin djg_google_xml_sitemaps
+     *
+     * @package wolf
+     * @subpackage djg_google_xml_sitemaps
+     *
+	 * @author Michał Uchnast <djgprv@gmail.com>,
+	 * @copyright kreacjawww.pl
+	 * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
+     */
+
+    return array(
+		'djg_google_xml_sitemaps' => 'djg_google_xml_sitemaps',
+		'[djg] XML sitemaps' => '[djg] XML sitemaps',
+		/* documentation */
+    	'Simple plugin to generate xml sitemap SEO compatible.' => 'Simple plugin to generate xml sitemap SEO compatible.',
+		'Djg Google Xml Sitemaps Documentation' => 'Djg Google Xml Sitemaps Documentations',
+		'Sitemap adress is:' => 'Sitemap adress is:',
+		'Read about sitemaps:' => 'Read about sitemaps:',
+		'Your sitemap:' => 'Your sitemap:',
+		'See example on:' => 'e example on:',
+		/* settings */
+		'Header' => 'Header',
+		'XML' => 'XML', // 1.1.8
+		'TXT' => 'TXT', // 1.0.8
+		'Choose header.' => 'Choose header.',
+		'HOMEPAGE' => 'HOMEPAGE',
+		'Add HOMEPAGE to xml tree.' => 'Add HOMEPAGE to xml tree.',
+		'STATUS HIDDEN' => 'STATUS HIDDEN',
+		'Add HIDDEN PAGES to xml tree.' => 'dd HIDDEN PAGES to xml tree.',
+		'LOGIN REQUIRED' => 'LOGIN REQUIRED',
+		'Add LOGIN REQUIRED PAGES to xml tree.' => 'Add LOGIN REQUIRED PAGES to xml tree.',
+		'Changefreq' => 'Changefreqy',
+		'Default changefreq for pages.' => 'fault changefreq for pages.',
+		'Priority' => 'Priority',
+		'Default priority for pages.' => 'Default priority for pages.',
+		'Robots.txt' => 'Robots.txt', // 1.1.8
+		'Contents of the robots.txt file.' => 'Contents of the robots.txt file.', // 1.1.8
+		/* select */
+		'always' => 'always',
+		'hourly' => 'hourlyę',
+		'daily' => 'dailye',
+		'weekly' => 'weekly',
+		'monthly' => 'monthly',
+		'yearly' => 'yearly',
+		'never' => 'ever',
+		/* 1.0.7 */
+		'Cache' => 'Cache',
+		'Set Yes if you want to caching sitemap file.' => 'et Yes if you want to caching sitemap file.',
+		'Clear cache (:date)' => 'Clear cache (:date)',
+		'Cache was cleared.' => 'Cache was cleared.',
+    );
---
+++ i18n/nl-message.php
@@ -0,0 +1,53 @@
+<?php
+
+    /**
+     * Dutch file for plugin djg_google_xml_sitemaps
+     *
+     * @package wolf
+     * @subpackage djg_google_xml_sitemaps
+     *
+	 * @author Fortron,
+	 * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
+     */
+
+    return array(
+		'djg_google_xml_sitemaps' => 'djg_google_xml_sitemaps'
+		'[djg] XML sitemaps' => '[djg] XML sitemaps',
+		/* documentation */
+    	'Simple plugin to generate xml sitemap SEO compatible.' => 'Plugin om een SEO vriendelijke XML sitemap te genereren.',
+		'Djg Google Xml Sitemaps Documentation' => 'Djg Google Xml Sitemaps Documentatie',
+		'Sitemap adress is:' => 'Sitemap adres is:',
+		'Read about sitemaps:' => 'Lees meer over sitemaps:',
+		'Your sitemap:' => 'Je Sitemap:',
+		'See example on:' => 'Zie een voorbeeld op:',
+		/* settings */
+		'Header' => 'Header',
+		'XML' => 'XML', // 1.1.8
+		'TXT' => 'TXT', // 1.0.8
+		'Choose header.' => 'Selecteer de header.',
+		'HOMEPAGE' => 'Homepage',
+		'Add HOMEPAGE to xml tree.' => 'Voeg de Homepage aan de Sitemap toe',
+		'STATUS HIDDEN' => 'Status Verborgen',
+		'Add HIDDEN PAGES to xml tree.' => 'Voeg Status Verborgen pagina\'s aan de Sitemap toe.',
+		'LOGIN REQUIRED' => 'Login Vereist',
+		'Add LOGIN REQUIRED PAGES to xml tree.' => 'Voeg Login Vereist pagina\'s aan de Sitemap toe.',
+		'Changefreq' => 'Frequentie',
+		'Default changefreq for pages.' => 'Standaard frequentie voor pagina\'s',
+		'Priority' => 'Prioriteit',
+		'Default priority for pages.' => 'Standaard prioriteit voor pagina\'s',
+		'Robots.txt' => 'Robots.txt', // 1.1.8
+		'Contents of the robots.txt file.' => 'Contents of the robots.txt file.', // 1.1.8
+		/* select */
+		'always' => 'altijd',
+		'hourly' => 'elk uur',
+		'daily' => 'dagelijks',
+		'weekly' => 'wekelijks',
+		'monthly' => 'maandelijks',
+		'yearly' => 'jaarlijks',
+		'never' => 'nooit',
+		/* 1.0.7 */
+		'Cache' => 'Cache',
+		'Set Yes if you want to caching sitemap file.' => 'Set Yes if you want to caching sitemap file.',
+		'Clear cache (:date)' => 'lear cache (:date)',
+		'Cache was cleared.' => 'Cache was cleared.'',
+    );
